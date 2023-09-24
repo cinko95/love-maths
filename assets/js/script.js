502c705548f7a1ac915294c09080c132f53dc226
cinko95 <>
2023-09-24 19:53:38 +0000

    fixed division game

--- assets/js/script.js
+++ assets/js/script.js
@@ -87,7 +87,7 @@ function calculateCorrectAnswer() {
         return [operand1 * operand2, "multiply"];
     } else if (operator === "-") {
         return [operand1 - operand2, "subtract"];
-    } else if (operator === "/") { // Handle division calculation
+    } else if (operator === "/") {
         return [operand1 / operand2, "division"];
     } else {
         alert(`Unimplemented operator ${operator}`);
